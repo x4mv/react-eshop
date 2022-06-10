6c7d7d5c09122a0a81c2dfc99f98d23fa7060ff9
x4mv <contactsscu@gmail.com>
2022-06-10 16:44:04 -0400

    Separate in components, containers and pages and adding styles

--- src/routes/app.jsx
+++ src/routes/app.jsx
@@ -1,23 +1,29 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from '../containers/Layout';
-import RecoveryPassword from '../containers/RecoveryPassword';
-import Login from '../containers/Login';
-import '../styles/global.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "../containers/Layout";
+import RecoveryPassword from "../containers/RecoveryPassword";
+import Home from "../pages/Home";
+import NotFound from "../pages/NotFound";
+import Login from "../pages/Login";
+import "../styles/global.css";
 
 const app = () => {
-    return (
-        <BrowserRouter>
-            <Layout>
-                <Routes>
-                    <Route exact path='/' element={<Home />}/>
-                    <Route exact path="/login" element={<Login />} />
-                    <Route exact path="RecoveryPassword" component={<RecoveryPassword />}/>
-                    <Route component={<NotFound />}/>
-                </Routes>
-            </Layout>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route
+            exact
+            path="/RecoveryPassword"
+            element={<RecoveryPassword />}
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
 };
 
 export default app;
